0550d3baae6db2c9bbea8338ecc8d573b7a2e79c
Vevis <p.veverka89@gmail.com>
2014-03-31 22:27:48 +0200

    tokbox

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -16,49 +16,53 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+    onDeviceReady: function () {
+
+        // Getting OpenTokRTC's room's credentials. 
+        // To use your own room in opentokrtc, change cordova to room of your choice
+        //   -> ie: https://opentokrtc.com/myroom.json
+        // To use your own credentials
+        //  replace data.apiKey, data.sid, and data.token with your own 
+        var apiKey = "44682012";
+        var sessionId = "2_MX40NDY4MjAxMn5-TW9uIE1hciAzMSAxMzowNjozOCBQRFQgMjAxNH4wLjYzMzcwNTh-";
+        var token = "T1==cGFydG5lcl9pZD00NDY4MjAxMiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1jMTg3Y2YzYjAxOTM5MDc5ZGY4YzRiZmE1YjAwNmJjYzlhOWE5ZDFhOnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9Ml9NWDQwTkRZNE1qQXhNbjUtVFc5dUlFMWhjaUF6TVNBeE16b3dOam96T0NCUVJGUWdNakF4Tkg0d0xqWXpNemN3TlRoLSZjcmVhdGVfdGltZT0xMzk2Mjk2NDAwJm5vbmNlPTAuNTY3MDI3Nzc2ODEzNDI3JmV4cGlyZV90aW1lPTEzOTYzODI4MDAmY29ubmVjdGlvbl9kYXRhPQ==";
+
+        // Very simple OpenTok Code
+        var publisher = TB.initPublisher(apiKey, 'myPublisherDiv');
+
+        var session = TB.initSession(sessionId);
+        session.on({
+            'sessionConnected': function (event) {
+                session.publish(publisher);
+            },
+            'streamCreated': function (event) {
+                var div = document.createElement('div');
+                div.setAttribute('id', 'stream' + event.stream.streamId);
+                document.body.appendChild(div);
+                session.subscribe(event.stream, div.id, { subscribeToAudio: false });
+            }
+        });
+        session.connect(apiKey, token);
+
     },
     // Update DOM on a Received Event
-    receivedEvent: function() {
-        $.get("https://opentokrtc.com/cordova.json", function( data ){
-            var publisher = TB.initPublisher(data.apiKey,'myPublisherDiv');
-
-            var session = TB.initSession( data.sid ); 
-            session.on({
-                'sessionConnected': function( event ){
-                    session.publish( publisher );
-                },
-                'streamCreated': function( event ){
-                    var div = document.createElement('div');
-                    div.setAttribute('id', 'stream' + event.stream.streamId);
-                    document.body.appendChild(div);
-                    session.subscribe( event.stream, div.id, {subscribeToAudio: false} );
-                }
-            });
-            session.connect( data.apiKey, data.token );
-        });
+    receivedEvent: function (id) {
     }
-         
-  
 };
-
-
-
