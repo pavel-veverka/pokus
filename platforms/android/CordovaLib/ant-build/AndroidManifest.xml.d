d9f98e4b5235e4ab1b26ed6adad40fa5ad7d5b48
Vevis <p.veverka89@gmail.com>
2014-04-02 23:10:27 +0200

    novy

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-E:\VUT FIT\_Magisterske studium\_Diplomka\AndroidDev\teamstorm\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-E:\VUT FIT\_Magisterske studium\_Diplomka\AndroidDev\teamstorm\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : E:\VUT FIT\_Magisterske studium\_Diplomka\AndroidDev\teamstorm\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
